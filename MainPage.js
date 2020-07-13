b4f33cca2930e2fb6a2a004320225991b8bb28ea
Nicolly21 <nicollyoliveira21@hotmail.com>
2020-07-13 20:40:33 -0300

    Criando Carousel

---
+++ MainPage.js
@@ -0,0 +1,8 @@
+//Utilizando o scroll para mudar imagens
+document.querySelector("#items").addEventListener("wheel", event => {
+    if(event.deltaY > 0){
+        event.target.scrollBy(300, 0);
+    } else {
+        event.target.scrollBy(-300, 0);
+    }
+});
